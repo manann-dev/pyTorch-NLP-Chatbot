3123427992dee1355ce64f6bb04e3127461bb540
manann-dev <manan5316@gmail.com>
2024-05-13 13:09:53 +0530

    chat

--- chat.py
+++ chat.py
@@ -29,3 +29,22 @@ while True:
     sentence = input('You: ')
     if sentence == "quit":
         break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X)
+
+    output = model(X)
+    _, predicted = torch.max(output,dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.75 :
+        for intent in intents["intents"]:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: I do not understand...")
